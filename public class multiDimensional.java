4e5aadc63eb3dbe68750c96eb4658e4eb79609c2
Sanjeevkumar2514 <107408977+Sanjeevkumar2514@users.noreply.github.com>
2022-06-17 14:55:56 +0530

    Update public class multiDimensional.java

--- public class multiDimensional.java
+++ public class multiDimensional.java
@@ -9,8 +9,7 @@ public class multiDimensional
 		for (int i=0; i< 3 ; i++)
 		{
 			for (int j=0; j < 3 ; j++)
-				System.out.print(arr[i][j] + " ");
-
+			System.out.print(arr[i][j] + " ");
 			System.out.println();
 		}
 	}
